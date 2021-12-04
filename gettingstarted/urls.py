a173f2f9423ccc0a57f67cf62fbd58ae432f514c
Chris Barker <bluquar@gmail.com>
2021-12-04 09:33:11 -0800

    remove db view

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -18,7 +18,6 @@ admin.autodiscover()
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('activate/<str:activation_key>', views.activate, name="activate"),

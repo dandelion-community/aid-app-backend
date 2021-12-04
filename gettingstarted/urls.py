2d826661001e5da5316bb708058387c99339e57c
Chris Barker <bluquar@gmail.com>
2021-12-04 09:22:30 -0800

    Update urls.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -18,6 +18,8 @@ admin.autodiscover()
 urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path("admin/", admin.site.urls),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]

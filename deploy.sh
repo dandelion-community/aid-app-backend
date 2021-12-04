8bc8d1a4d3e6caccdc7277bfab29e9e6941b6d87
Chris Barker <bluquar@gmail.com>
2021-12-04 01:28:33 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,10 @@ APP_NAME=$1
 
 heroku run -a $APP_NAME python manage.py migrate auth 
 heroku run -a $APP_NAME python manage.py migrate 
-heroku run -a $APP_NAME python manage.py collectstatic
-heroku run -a $APP_NAME python manage.py createsuperuser
+
+# Looks like this one is not needed -- staging deployment rendered
+# css fine on the admin UI without running this command.
+# heroku run -a $APP_NAME python manage.py collectstatic
+
+# Only needed for new deployments.
+# heroku run -a $APP_NAME python manage.py createsuperuser

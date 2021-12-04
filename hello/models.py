77ae93601ab2a41372c0c1581be42a00327f9829
Chris Barker <bluquar@gmail.com>
2021-12-04 01:20:47 -0800

    add new model

--- hello/models.py
+++ hello/models.py
@@ -2,16 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class AnonymousPerson(models.Model):
-    number = models.AutoField(primary_key=True)
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        blank=True,
-        null=True
-    )
-
-
 class AidRequest(models.Model):
     what_is_needed = models.CharField(max_length=1000)
     completed = models.BooleanField(default=False)
@@ -24,18 +14,4 @@ class AidRequest(models.Model):
     who_recorded_it_username = models.CharField(
         max_length=100,
         null=True,
-    )
-    who_is_it_for = models.ForeignKey(
-        AnonymousPerson,
-        on_delete=models.DO_NOTHING,
-        related_name='requests_for',
-        blank=True,
-        null=True,
-    )
-    who_recorded_it = models.ForeignKey(
-        AnonymousPerson,
-        on_delete=models.DO_NOTHING,
-        related_name='requests_recorded_by',
-        blank=True,
-        null=True
     )
